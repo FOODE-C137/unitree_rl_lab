9a16146d6e0ebec87530a1631b35b408d1d2e6b3
Zhang <boyu.zhang@tum.de>
2026-06-05 11:28:41 +0200

    delete external force

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/go2/go2_marg_oracle_risk_terrain_env_cfg.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/go2/go2_marg_oracle_risk_terrain_env_cfg.py
@@ -197,32 +197,6 @@ def randomize_motor_strength(
             )
 
 
-def apply_external_force_torque_xy(
-    env,
-    env_ids: torch.Tensor | None,
-    asset_cfg: SceneEntityCfg,
-    force_xy_range: tuple[float, float],
-    torque_z_range: tuple[float, float] = (0.0, 0.0),
-):
-    """Apply a persistent horizontal disturbance force and yaw torque to selected bodies."""
-
-    asset = env.scene[asset_cfg.name]
-    if env_ids is None:
-        env_ids = torch.arange(env.scene.num_envs, device=asset.device)
-
-    num_bodies = len(asset_cfg.body_ids) if isinstance(asset_cfg.body_ids, list) else asset.num_bodies
-    forces = torch.zeros((len(env_ids), num_bodies, 3), device=asset.device)
-    torques = torch.zeros_like(forces)
-
-    forces[..., :2] = math_utils.sample_uniform(
-        force_xy_range[0], force_xy_range[1], (len(env_ids), num_bodies, 2), asset.device
-    )
-    torques[..., 2] = math_utils.sample_uniform(
-        torque_z_range[0], torque_z_range[1], (len(env_ids), num_bodies), asset.device
-    )
-    asset.set_external_force_and_torque(forces, torques, env_ids=env_ids, body_ids=asset_cfg.body_ids)
-
-
 def reset_base_with_terrain_orientation(
     env,
     env_ids: torch.Tensor | None,
@@ -355,12 +329,12 @@ class EventCfg:
 
     # reset
     base_external_force_torque = EventTerm(
-        func=apply_external_force_torque_xy,
+        func=mdp.apply_external_force_torque,
         mode="reset",
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names="base"),
-            "force_xy_range": (-8.0, 8.0),
-            "torque_z_range": (-1.0, 1.0),
+            "force_range": (0.0, 0.0),
+            "torque_range": (-0.0, 0.0),
         },
     )
 
